8c49b636b6c128b4ff6dcdc08c0446321d76edec
Angel Barrientos <uetiko@gmail.com>
2013-08-20 01:09:09 -0500

    testing with json and simple query dql

--- src/calavera/customerBundle/Controller/NewsletterController.php
+++ src/calavera/customerBundle/Controller/NewsletterController.php
@@ -3,20 +3,43 @@
 namespace calavera\customerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class NewsletterController extends Controller{
+class NewsletterController extends Controller {
 
-    public function indexAction(){
+    public function indexAction() {
         return $this->render('calaveracustomerBundle:Newsletter:index.html.twig');
     }
 
-    public function signAction(){
+    public function signAction() {
         $bo = new \calavera\customerBundle\BO\NewsletterBO($this->getDoctrine()->getEntityManager());
     }
-    
-    public function unsignAction(){
+
+    public function unsignAction() {
         
     }
+
+    public function testAction() {
+        $em = $this->getDoctrine()->getEntityManager();
+        $result = new \calavera\customerBundle\Entity\CatNewsletterStatus();
+        
+        $dql = "select u from \calavera\customerBundle\Entity\CatNewsletterStatus u";
+        try{
+            $query = $em->createQuery($dql);
+            $result = $query->getResult();
+        }catch ( \Exception $e){
+            
+        }
+        $a = array(
+            "datoUno" => $result[0]->getStatus(),
+            "datoDos" => $result[1]->getStatus()
+        );
+        
+        $response = new Response(json_encode($a));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
 }
